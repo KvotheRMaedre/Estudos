3a60e6f87986349f98335be8fcd3ba0467dff74b
Ítalo H <italodssouza.backup@gmail.com>
2023-02-13 13:59:48 -0300

    Update index.js
    Deixando a resposta como json

--- Node/hello-world-expressjs/index.js
+++ Node/hello-world-expressjs/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send('Oi, meus lindos')
+    let resposta = {mensagem: 'Oi, meus lindos'}
+    res.json(resposta);
 });
 
 app.listen(8080, () => {
